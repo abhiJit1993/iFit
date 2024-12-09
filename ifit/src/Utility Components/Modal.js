d16785216d4711e8fc48557e7986cf24eba3f992
abhiJit1993 <48312391+abhiJit1993@users.noreply.github.com>
2024-12-09 07:44:43 +0000

    Formatting

--- ifit/src/Utility Components/Modal.js
+++ ifit/src/Utility Components/Modal.js
@@ -1,23 +1,25 @@
 import { Modal } from "antd";
 
 
-function IFitModal (props){
-    return(
+function IFitModal(props) {
+    return (
         <Modal
-        title={props.title}
-        open={props.isOpen}
-        onOk={props.onOk}
-        onCancel={props.onCancel}
-        cancelText = {props.cancelText ? props.cancelText : 'Cancel'}
-        okText={props.okText ?  props.okText : "OK"}
-        maskClosable={props.maskClosable ? props.maskClosable : false}
-        footer={props.footer?props.footer:null}
-        closable={false}
+            title={props.title}
+            open={props.isOpen}
+            onOk={props.onOk}
+            onCancel={props.onCancel}
+            cancelText={props.cancelText ? props.cancelText : 'Cancel'}
+            okText={props.okText ? props.okText : "OK"}
+            maskClosable={props.maskClosable ? props.maskClosable : false}
+            footer={props.footer ? props.footer : null}
+            closable={false}
         >
-         <div>{props.Component()}</div>
-         
+            <div>
+
+                {props.Component()}</div>
+
         </Modal>
     )
-     
-    }
-    export default IFitModal;
+
+}
+export default IFitModal;
